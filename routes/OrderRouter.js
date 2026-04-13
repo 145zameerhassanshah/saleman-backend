44c8c2a7ed1b10ae0cb8ab597892d07124bbc42a
Faisal-hussain1 <fsl687bt@gmail.com>
2026-04-13 15:52:42 +0500

    updated

--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -11,14 +11,16 @@ router.get(
     USER_ROLES.SALESMAN,
     USER_ROLES.SUPER_ADMIN,
     USER_ROLES.DISPATCHER,
-    USER_ROLES.ACCOUNTANT
+    USER_ROLES.ACCOUNTANT,
+    USER_ROLES.MANAGER
+
   ),
   orderController.getDashboardStats
 );
-router.get("/:id", roleMiddleware(USER_ROLES.ADMIN,USER_ROLES.SALESMAN,USER_ROLES.SUPER_ADMIN,USER_ROLES.DISPATCHER,USER_ROLES.ACCOUNTANT), orderController.showAll);
+router.get("/:id", roleMiddleware(USER_ROLES.ADMIN,USER_ROLES.SALESMAN,USER_ROLES.SUPER_ADMIN,USER_ROLES.DISPATCHER,USER_ROLES.ACCOUNTANT,USER_ROLES.MANAGER), orderController.showAll);
 router.post("/",roleMiddleware(USER_ROLES.ADMIN,USER_ROLES.SALESMAN), orderController.store);
 router.patch("/update-status/:id",orderController.updateOrderStatus);
-router.patch("/:id",roleMiddleware(USER_ROLES.ADMIN,USER_ROLES.SALESMAN,USER_ROLES.DISPATCHER,USER_ROLES.ACCOUNTANT), orderController.update);
+router.patch("/:id",roleMiddleware(USER_ROLES.ADMIN,USER_ROLES.SALESMAN,USER_ROLES.DISPATCHER,USER_ROLES.ACCOUNTANT,USER_ROLES.MANAGER), orderController.update);
 router.delete("/:id",roleMiddleware(USER_ROLES.ADMIN,USER_ROLES.SALESMAN), orderController.remove);
 router.get(
   "/products/:categoryId",
